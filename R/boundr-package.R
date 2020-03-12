161325ec4ad4ba6b27e169c9e1d0a23f35bf4471
Karim Naguib <karimn2.0@gmail.com>
2020-03-12 17:12:55 -0400

    Preparing boundr package

---
+++ R/boundr-package.R
@@ -0,0 +1,17 @@
+#' The 'boundr' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name boundr-package
+#' @aliases boundr
+#' @useDynLib boundr, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2019). RStan: the R interface to Stan. R package version 2.19.2. https://mc-stan.org
+#' Pearl, J. (2009). Causality: Models, Reasoning and Inference (Second Ed). Cambridge: Cambridge University Press.
+#'
+NULL
